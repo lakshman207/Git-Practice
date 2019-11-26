bbd362613026dc01c188be2029c9aafcc30977dc
macmini_home computer <macmini@macmini-homes-mac-mini.local>
2019-11-26 10:38:48 +0530

    Initial Commit

---
+++ Git Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Practice
+//
+//  Created by Home Computer on 26/11/2019.
+//  Copyright © 2019 Home Computer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
